b4e2e25dae23483b0ce60b2936f1b57558ee16e8
M. P. McDonald <mpmcd@uw.edu>
2016-07-06 08:45:52 -0600

    corrections and suggestions

--- Practice-Writing Functions.R
+++ Practice-Writing Functions.R
@@ -13,5 +13,24 @@ sum_divide_sd <- function(x) {
   first_add <- sum(x)
   second_sd <- sd(x)
   third_divide <- first_add/second_sd
+  # Within a function, you can print things to the console.  This can help you
+  # see what's going on...(I typically remove these after I'm done debugging)
+  print(first_add)
+  print(second_sd)
+  print(third_divide)
+  # Looks like everything is working, it's just not giving you what you want.  Why?
+  # Functions have to return things - and they can only return one "object" (ish)
+  # .. here we're binding the results into one object
+  results <- c(first_add, second_sd, third_divide)
+  names(results) <- c("sum", "sd", "sum/sd")
+  return(results)
+  # Note, I didn't have to use "return", I could've just typed "results", but it's better to use
+  # "return"
 }
 #why isn't this working? hmm...
+
+data <- rnorm(10, 2, .1)
+sum_divide_sd(data)
+my_results <- sum_divide_sd(data)
+my_results
+my_results[1]
