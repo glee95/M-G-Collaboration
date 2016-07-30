3cb23b4d3d7009767c35cc80cea76dcca2ec5c2c
M. P. McDonald <mpmcd@uw.edu>
2016-07-29 22:28:33 -0700

    Add suggestions about by block summary statistics

--- Race IAT Data Analysis.R
+++ Race IAT Data Analysis.R
@@ -5,3 +5,7 @@ latency <- select(RACE.IAT_raw_1, latency)
 summarise(latency, min = min(latency), max = max(latency), avg = mean(latency), med = median(latency), sd = sd(latency), var = var(latency), IQR = IQR(latency), range = diff(range(latency)))
 
 #everything here makes sense except i'm not exactly sure of the functionality in this scenario
+
+iat %>% group_by(subject, blocknum) %>%
+  summarise(meanLatency = mean(latency),
+            sdLatency = sd(latency))
